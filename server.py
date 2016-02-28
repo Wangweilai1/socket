387f8c80210412c171ff8139d049d61e317f054b
wangweilai <1689977424@qq.com>
2016-02-28 10:10:59 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,114 @@
 // server.cpp : Defines the entry point for the console application.
 //
-
 #include "stdafx.h"
 #include <winsock2.h>
 #include <Windows.h>
-#pragma comment (lib, "ws2_32.lib")  //加载 ws2_32.dll
+#include<conio.h>
 
-int main(){
-    //初始化 DLL
+#pragma comment (lib, "ws2_32.lib")   //加载 ws2_32.dll
+
+int g_postion = 1;
+CRITICAL_SECTION gcc;
+
+void gotoxy(int x, int y);
+COORD get_xy();
+void ClearScreen(void);
+void ClearScreen1(void);
+DWORD WINAPI  sendCustmer(LPVOID);
+DWORD WINAPI  displayServer(LPVOID);
+
+
+int main()
+{
+    system("cls");
+    HANDLE    hThread1 = NULL;
+    HANDLE    hThread2 = NULL;
+    HANDLE hOut;
+    hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    WORD att = FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_INTENSITY|BACKGROUND_BLUE;
+    SetConsoleTextAttribute(hOut, att);
+    SMALL_RECT rc = {0, 0, 80, 25};
+    SetConsoleWindowInfo(hOut, true,&rc);
+    SetConsoleTitle(L"The Chat Tools ");
+    ClearScreen();
+    InitializeCriticalSection(&gcc);
+    hThread1 = CreateThread(NULL, NULL, displayServer, NULL, NULL, NULL);
+    hThread2 = CreateThread(NULL, NULL, sendCustmer, NULL, NULL, NULL);
+    HANDLE    tHandle[] = {hThread1, hThread2};
+    WaitForMultipleObjects(2, tHandle, true, INFINITE);
+    DeleteCriticalSection(&gcc);
+
+    WSACleanup();
+    CloseHandle(hThread1);
+    CloseHandle(hThread2);
+
+}
+
+
+
+DWORD WINAPI sendCustmer(LPVOID lpParameter)
+{
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    //向服务器发起请求
+    sockaddr_in sockAddr;
+    memset(&sockAddr, 0, sizeof(sockAddr));  //每个字节都用0填充
+    sockAddr.sin_family = PF_INET;
+    sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    //sockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    sockAddr.sin_port = htons(4321);
+    char SendBuffer[MAXBYTE];
+
+    while (1)
+    {
+        SOCKET sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+        do 
+        {
+            if (0 == connect(sock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR)))
+            {
+                break;
+            }
+        } while (true);
+        memset(SendBuffer, 0, MAXBYTE);
+
+        gotoxy(0, 21);
+        ClearScreen1();
+#if 0
+        for (int j = 0; j < 100; ++j)
+        {
+            printf_s(" ");
+        }
+#endif
+        do 
+        {
+            gotoxy(0, 21);
+            gets_s (SendBuffer);
+        } while ( 0 == strlen(SendBuffer) );
+        gotoxy(0, g_postion);
+        if (g_postion < 20)
+        {
+            ++g_postion;
+        }
+        else
+        {
+            g_postion = 1;
+            gotoxy(0, g_postion);
+            ++g_postion;
+            ClearScreen();
+            //system("cls");
+        }
+        printf_s("I'm: %s\n", SendBuffer);
+        send(sock, SendBuffer, strlen(SendBuffer) + sizeof(char), NULL);
+        shutdown(sock, SD_SEND);
+        closesocket(sock);
+    }
+    return 0;
+}
+
+
+DWORD WINAPI displayServer(LPVOID lpParameter)
+{
     WSADATA wsaData;
     WSAStartup( MAKEWORD(2, 2), &wsaData);
 
@@ -15,33 +116,45 @@ int main(){
     SOCKET servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
     //绑定套接字
-    sockaddr_in sockAddr;
-    memset(&sockAddr, 0, sizeof(sockAddr));  //每个字节都用0填充
-    sockAddr.sin_family = PF_INET;  //使用IPv4地址
-    sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");  //具体的IP地址
-    sockAddr.sin_port = htons(1234);  //端口
+    sockaddr_in  sockAddr;
+    memset(&sockAddr, 0, sizeof(sockAddr));                  //每个字节都用0填充
+    sockAddr.sin_family = PF_INET;                           //使用IPv4地址
+    sockAddr.sin_addr.s_addr = htonl(INADDR_ANY);            //自动获取IP地址
+    sockAddr.sin_port = htons(1234);                         //端口
     bind(servSock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR));
-    printf_s("****** Welcome To Use TELE,This Server ******\n\n");
+
     //进入监听状态
     listen(servSock, 20);
 
     //接收客户端请求
     SOCKADDR clntAddr;
     int nSize = sizeof(SOCKADDR);
-    SOCKET clntSock = accept(servSock, (SOCKADDR*)&clntAddr, &nSize);
-    char szBuffer[MAXBYTE];
-    char str[MAXBYTE];
-
+    SOCKET clntSock;
+    char RecvBuffer[MAXBYTE];
+    COORD old_postion = {0, 0};
     while (1)
     {
-        memset(szBuffer, 0, MAXBYTE);
-        recv(clntSock, szBuffer, MAXBYTE, NULL);
-        printf("Customer: %s\n", szBuffer);
-        //向客户端发送数据
-        memset(str, 0, MAXBYTE);
-        printf_s("Server: ");
-        gets_s(str);
-        send(clntSock, str, strlen(str)+sizeof(char), NULL);
+        clntSock = accept(servSock, (SOCKADDR*)&clntAddr, &nSize);
+        memset(RecvBuffer, 0, MAXBYTE);
+       do 
+       {
+           recv(clntSock, RecvBuffer, MAXBYTE, NULL);
+       } while (0 == strlen(RecvBuffer));
+        old_postion = get_xy();
+        if (g_postion < 20)
+        {
+            gotoxy(0, g_postion++);
+        }
+        else
+        {
+            g_postion = 1;
+            gotoxy(0, g_postion);
+            ++g_postion;
+            ClearScreen();
+            //system("cls");
+        }
+        printf_s("You: %s\n", RecvBuffer);
+        gotoxy(old_postion.X, old_postion.Y);
     }
 
     //关闭套接字
@@ -53,3 +166,50 @@ int main(){
     system("pause");
     return 0;
 }
+
+void gotoxy(int x,int y)
+{
+    HANDLE hOutput;
+    COORD loc;
+    loc.X = x;
+    loc.Y = y;
+    hOutput= GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(hOutput, loc);
+}
+
+COORD get_xy()
+{
+    COORD postion = {0, 0};
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    GetConsoleScreenBufferInfo( hStdOut, &csbi );
+    postion = csbi.dwCursorPosition;
+    return postion;
+}
+void ClearScreen(void)
+{
+    HANDLE hOut;
+    DWORD written;
+    hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO bInfo;
+    GetConsoleScreenBufferInfo( hOut, &bInfo ); 
+    COORD home = {0, 0};
+    WORD att = bInfo.wAttributes;
+    unsigned long size = bInfo.dwSize.X * 21;
+    FillConsoleOutputAttribute(hOut, att, size, home, &written);
+    FillConsoleOutputCharacter(hOut, ' ', size, home, &written);
+}
+void ClearScreen1(void)
+{
+    HANDLE hOut;
+    DWORD written;
+    hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO bInfo;
+    GetConsoleScreenBufferInfo( hOut, &bInfo ); 
+    COORD home = {0, 21};
+    WORD att = bInfo.wAttributes;
+    unsigned long size = bInfo.dwSize.X  * (bInfo.dwSize.Y - 21);
+    FillConsoleOutputAttribute(hOut, att, size, home, &written);
+    FillConsoleOutputCharacter(hOut, ' ', size, home, &written);
+}
+
